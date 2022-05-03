931d56723f8ad35f26d582c06b2e064b5d1aa523
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-03 11:58:12 -0700

    wrote initial character list with loading

--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
-export default function List() {
+
+export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+
+  useEffect(() => {
+    const getCharacters = async () => {
+
+      const res = await fetch('https://rickandmortyapi.com/api/character');
+      const { data } = await res.json();
+      const characterData = data.map((character) => ({
+        id: character.id,
+        name: `${character.name}`
+      }));
+      setCharacters(characterData);
+      setLoading(false);
+
+    };
+    getCharacters();
+  }, []);
+
   return (
-    <div>CharacterList</div>
+    <>
+    <h1>List of Characters</h1>
+    {loading ? (
+      <p>Loading</p>
+    ) : (
+      <>
+      <ul>
+        {characters.map(() => {
+          return (
+            <li key={character.id}>
+              {character.name}
+            </li>
+          )
+        })}
+      </ul>
+      </>
+    )}
+    </>
   )
 }
