88ec35bc796f3bf69314bbc56330c8055ae2667b
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-03 12:20:46 -0700

    added useLocation in list

--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const status = new URLSearchParams(location.search).get('status') ?? 'all';
+  const location = useLocation();
+
   
 
   useEffect(() => {
     const getCharacters = async () => {
+      setLoading(true);
 
-      const res = await fetch('https://rickandmortyapi.com/api/character');
-      const { data } = await res.json();
-      const characterData = data.map((character) => ({
-        id: character.id,
-        name: `${character.name}`
-      }));
-      setCharacters(characterData);
+      const statusParams = new URLSearchParams(location.search).get('status');
+
+      const url = status === 'all' || !status 
+      ? 'https://rickandmortyapi.com/api/character'
+      : `https://rickandmortyapi.com/api/character?status=${statusParams}`;
+      const res = await fetch(url);
+      const { results } = await res.json();
+      setCharacters(results);
       setLoading(false);
 
     };
     getCharacters();
-  }, []);
+  }, [location.search]);
 
   return (
     <>
@@ -30,7 +36,7 @@ export default function CharacterList() {
     ) : (
       <>
       <ul>
-        {characters.map(() => {
+        {characters.map((character) => {
           return (
             <li key={character.id}>
               {character.name}
